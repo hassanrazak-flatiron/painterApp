ca322529fd2383766fb50d682ce509d34dbab81e
Hassan Razak <hassanrazak1991@gmail.com>
2022-12-30 13:53:08 -0500

    ColorBar and Canvas subcomps created and linked to App.js

--- painterapp/src/App.js
+++ painterapp/src/App.js
@@ -1,9 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import ColorBar from './ColorBar';
+import Canvas from './Canvas';
 
 function App() {
   return (
     <div className="App">
+      <ColorBar />
+      <Canvas />
     </div>
   );
 }
