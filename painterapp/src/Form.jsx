1e34f4f2695ad5a48c7e12f9ed892690f85b7074
Hassan Razak <hassanrazak1991@gmail.com>
2023-01-01 18:42:19 -0500

    update more header styling

--- painterapp/src/Form.jsx
+++ painterapp/src/Form.jsx
@@ -8,9 +8,9 @@ const Form = () => {
                     <label>Stencil</label><br></br>
                     <input className = "font-link"placeholder="Enter a stencil"></input><br></br>
                     <label>Brush</label><br></br>
-                    <input placeholder="Enter a Brush"></input><br></br>
+                    <input className = "font-link" placeholder="Enter a Brush"></input><br></br>
                     <label className = "font-link">Texture</label><br></br>
-                    <input placeholder="Enter Texture"></input><br></br>
+                    <input className = "font-link" placeholder="Enter Texture"></input><br></br>
                     <input className = "font-link" type='submit'></input>
                 </form>
             </div>         
@@ -18,9 +18,9 @@ const Form = () => {
             
 
             <div>
-                <p id ='contact' className="font-link"><em>Sebasitan Otaola</em></p>
-                <p id ='contact' className="font-link"><em>Hassan Razak</em></p>
-                <p id ='contact' className="font-link"><em>Contact Us</em></p>
+                <p id ='contact' className="font-link"><span id='three'><em>Sebasitan Otaola</em></span></p>
+                <p id ='contact' className="font-link"><span id='three'><em>Hassan Razak</em></span></p>
+                <p id ='contact' className="font-link"><span id = 'three'><em>Contact Us</em></span></p>
             </div>
         </div>
      );
