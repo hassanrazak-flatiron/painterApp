040c37f6d6b95a49f0c7d141a282185adff204db
Hassan Razak <hassanrazak1991@gmail.com>
2023-01-01 16:42:13 -0500

    added paint font' git commit -m 'added paint font

--- painterapp/src/Canvas.jsx
+++ painterapp/src/Canvas.jsx
@@ -3,7 +3,7 @@ import './App.css'
 const Canvas = () =>{
     return (
         <div id = "canvas">
-            <p>Canvas</p>
+          
             
 
         </div>
--- painterapp/src/Header.jsx
+++ painterapp/src/Header.jsx
@@ -2,6 +2,7 @@ const Header = () => {
     return ( 
         <div id = 'header' >
             <h1 className = "font-link">P A I N T</h1>
+
            
         </div>
      );
