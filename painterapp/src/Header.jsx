d237223b371e3020e143f050d3f171d4fa3f18ee
Hassan Razak <hassanrazak1991@gmail.com>
2023-01-01 16:59:12 -0500

    added new font scheme to header

--- painterapp/src/Header.jsx
+++ painterapp/src/Header.jsx
@@ -1,7 +1,11 @@
 const Header = () => {
     return ( 
         <div id = 'header' >
-            <h1 className = "font-link">P A I N T</h1>
+            <h1 id='one'className = "font-link">P <span id='two'>A</span> <span id="three">I</span> <span id='four'>N</span> <span id='five'>T</span></h1>
+            <h1 id='two' className = "font-link">A <span id="three">I</span> <span id='four'>N</span> <span id='five'>T</span></h1>
+            <h1 id='three'className = "font-link">I <span id='four'>N</span> <span id='five'>T</span></h1>
+            <h1 id='four'className = "font-link">N <span id='five'>T</span></h1>
+            <h1 id='five' className = "font-link">T</h1>
 
            
         </div>
