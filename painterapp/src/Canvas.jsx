7c81e311065ccd56e6403f575dc809416bee095e
Hassan Razak <hassanrazak1991@gmail.com>
2023-01-03 15:46:24 -0500

    design layout update

--- painterapp/src/Canvas.jsx
+++ painterapp/src/Canvas.jsx
@@ -1,6 +1,20 @@
 import './App.css'
+import {useState,useEffect} from 'react'
 
 const Canvas = () =>{
+
+    const [metArray,setMetArray] = useState([])
+    const fetchMet = async () =>{
+        let req  = await fetch('https://collectionapi.metmuseum.org/public/collection/v1/objects')
+        let res = await req.json()
+        setMetArray(res)
+    }
+
+    useEffect(()=>{
+        fetchMet()
+
+    },[])
+
     return (
         <div id = "canvas">
           
